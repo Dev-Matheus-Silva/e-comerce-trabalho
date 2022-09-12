554d35e983a50bf06e39df2f82081ac7758b6725
Dev-Matheus-Silva <matheussilva0226@yahoo.com>
2022-09-12 17:12:24 -0300

    alteração

--- back/Auth/src/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/user');
-
-router.post('/', controller.post);
-router.post('/login', controller.login);
-
-module.exports = router;
